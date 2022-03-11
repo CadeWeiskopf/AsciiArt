6bf646f83e8a2b415b1a3de2d2e98f8713b9b065
Cade Weiskopf <weiskopfcd@gmail.com>
2022-03-11 12:47:20 -1000

    Added option to invert brightness characters

--- AsciiArt.py
+++ AsciiArt.py
@@ -7,6 +7,13 @@ pixels = list(img.getdata())
 height = img.height
 width = img.width
 brightnessCharacters = '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+brightnessCharactersBackwards = ''
+j = len(brightnessCharacters) - 1
+while j >= 0:
+    brightnessCharactersBackwards += brightnessCharacters[j]
+    j -= 1
+
+#brightnessCharacters = brightnessCharactersBackwards
 
 print('height=' + str(height) + ' width=' + str(width))
 print('length of pixels=' + str(len(pixels)))
